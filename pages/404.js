b8d064a08ba13370d931e538442777c160037e0d
Rahul Sreekumar <rahulsreekumar81@gmail.com>
2022-05-16 13:20:27 +0530

    user redirection using useRouter(from next) and useEffect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 const ErrorPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    // console.log("UseEffect has been runned");
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Oopss...</h1>
