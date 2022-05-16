eff93c33ae213731c34104b00b4b432ef9a50578
Rahul Sreekumar <rahulsreekumar81@gmail.com>
2022-05-16 20:22:55 +0530

    Developmet :final commit

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,31 +1,45 @@
 import Head from "next/head";
-
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
+  // map data to an array of path objects with params (id)
   const paths = data.map((ninja) => {
     return {
       params: { id: ninja.id.toString() },
     };
   });
+
   return {
     paths,
     fallback: false,
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <>
       <Head>
-        <title>Ninja Listing | Details</title>
-        <meta name="keywords" content="ninja" />
+        <title>Ninja Listing | Ninja</title>
       </Head>
       <div>
-        <h1>Details Page</h1>
+        <h1>{ninja.name}</h1>
+        <p>{ninja.email}</p>
+        <p>{ninja.website}</p>
+        <p>{ninja.address.city}</p>
       </div>
     </>
   );
 };
+
 export default Details;
