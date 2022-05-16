abaadb3aeab07bff4bddb64dc4fa753cb45f96e2
Rahul Sreekumar <rahulsreekumar81@gmail.com>
2022-05-16 19:44:27 +0530

    Development: Head section

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 export const getStaticPaths = async () => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
   const data = await res.json();
@@ -15,9 +17,15 @@ export const getStaticPaths = async () => {
 
 const Details = () => {
   return (
-    <div>
-      <h1>Details Page</h1>
-    </div>
+    <>
+      <Head>
+        <title>Ninja Listing | Details</title>
+        <meta name="keywords" content="ninja" />
+      </Head>
+      <div>
+        <h1>Details Page</h1>
+      </div>
+    </>
   );
 };
 export default Details;
